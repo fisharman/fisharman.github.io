572222d3dbbb511dd7fdf58c5407a5cedafaf33d
Ray Zhou <zhou.ray@gmail.com>
2018-05-23 22:50:54 -0700

    reduced image size

--- src/components/ImportImg.js
+++ src/components/ImportImg.js
@@ -1,13 +1,13 @@
-import flairCaption from '../images/experience/flairCaption.png';
+import flairCaption from '../images/experience/flairCaption.jpg';
 import ntcCaption from '../images/experience/ntcCaption.jpg';
 import paletteCaption from '../images/experience/paletteCaption.jpg';
 import bookviewerCaption from '../images/experience/bookviewerCaption.jpg';
-import salarypredictorCaption from '../images/experience/salarypredictorCaption.png';
+import salarypredictorCaption from '../images/experience/salarypredictorCaption.jpg';
 import instasearchCaption from '../images/experience/instasearchCaption.jpg';
 
 import flairModal from '../images/experience/flairModal.jpg';
 import ntcModal from '../images/experience/ntcModal.jpg';
-import paletteModal from '../images/experience/paletteModal.png';
+import paletteModal from '../images/experience/paletteModal.jpg';
 import bookviewerModal from '../images/experience/bookviewerModal.png';
 import instasearchModal from '../images/experience/instasearchModal.jpg';
 
